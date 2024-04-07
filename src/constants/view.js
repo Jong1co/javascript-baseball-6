9125e9e47ba4f5d5d3f3ef599d84e335a259eb99
Jong1co <narira0922@gmail.com>
2024-04-07 09:02:23 +0900

    feat: 상수 추가

---
+++ src/constants/view.js
@@ -0,0 +1,20 @@
+const START_GAME = "숫자 야구 게임을 시작합니다.";
+const ENTER_NUMBER = "숫자를 입력해주세요 : ";
+const COMPLETE_GAME = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+const RESTART_OR_EXIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+const BALL = "볼";
+const STRIKE = "스트라이크";
+const NOTHING = "낫싱";
+const END_GAME = "게임을 종료합니다.";
+
+export const VIEW = {
+  START_GAME,
+  ENTER_NUMBER,
+  COMPLETE_GAME,
+  RESTART_OR_EXIT,
+  BALL,
+  STRIKE,
+  NOTHING,
+  END_GAME,
+};
